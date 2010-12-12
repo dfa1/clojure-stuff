e7f3fc2e29f7746509390e4c2f045632e16f38f7
Davide Angelocola <davide.angelocola@gmail.com>
2010-12-12 23:57:42 +0100

    improved Euclidean norm

---
+++ norm.clj
@@ -0,0 +1,26 @@
+(ns norm)
+
+(use 'clojure.test)
+(import java.lang.Math)
+
+(defn square [n]
+  "Return the square of a number."
+  (* n n))
+
+(defn squares [coll]
+  "Return a lazy seq whose values are the square of coll."
+  (map square coll))
+   
+(defn sum [coll]
+  "Return the sum of the values of coll."
+  (reduce + coll))
+
+(defn norm [& values]
+  "Return the Euclidean distance between points."
+  (Math/sqrt (sum (squares values))))
+
+(is (= (norm) 0))
+(is (= (norm 2) 2))
+(is (= (norm 3 4) 5))
+(is (= (norm 3 3 3 3) 6))
+
