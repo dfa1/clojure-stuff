c9903a3202cd8694642f2623e5e1bae89842229e
Davide Angelocola <davide.angelocola@gmail.com>
2010-12-12 23:32:48 +0100

    merged lazy-fib.clj into fibonacci.clj

--- fibonacci.clj
+++ fibonacci.clj
@@ -31,13 +31,19 @@
       (recur next (+ current next) (- n 1))))]
     (inner-fib 0 1 n)))
 
-(def algorithms 
-     [ 
-      fibonacci 
-      tail-fibonacci
-      tail-fibonacci-improved
-      ])
+(defn- next-pair [[v1 v2]]
+  "Return the next Fibonacci pair"
+  [v2 (+ v1 v2)])
 
-;; benchmarking
-(map #(time (% 15)) algorithms)
+(defn lazy-fibonacci-seq []
+  "Return a lazy sequence of Fibonacci numbers."
+  (map first (iterate next-pair [0 1])))
 
+(defn lazy-fibonacci [n]
+  (nth (lazy-fibonacci-seq) n))
+
+;; benchmarks
+(def algorithms
+     [fibonacci tail-fibonacci tail-fibonacci-improved lazy-fibonacci])
+
+(map #(time (% 20)) algorithms)
