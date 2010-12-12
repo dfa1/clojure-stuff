1d46c101043e8377404d11c15d9500ccf75039ac
Davide Angelocola <davide.angelocola@gmail.com>
2010-12-12 20:31:01 +0100

    cosmetics

--- lazy-fib.clj
+++ lazy-fib.clj
@@ -1,12 +1,12 @@
-; my fib
+;; lazy fib
 (defn- next-pair [[v1 v2]]
   [v2 (+ v1 v2)])
-
-(next-pair [10 11])
-
-(last (take 5 (iterate next-pair [0 1])))
 
 (defn lazy-fib []
   (iterate next-pair [0 1]))
 
+;; tests
+(next-pair [10 11])
+(last (take 5 (iterate next-pair [0 1])))
 (take 10 (lazy-fib))
+
