210bc6a532e73aef3f458e14e0a54c995c1c88a8
Davide Angelocola <davide.angelocola@gmail.com>
2010-12-12 11:56:26 +0100

    coin toss example from programming clojure

---
+++ coin-toss.clj
@@ -0,0 +1,22 @@
+; random coin toss results
+(defn lazy-random [max]
+  (repeatedly #(rand-int max)))
+
+(take 10 (lazy-random 6))
+
+(defn- toss [n]
+  (cond
+   (= n 0) :head
+   (= n 1) :crux))
+(toss 1)
+
+(defn tosses [n]
+  (map toss (take n (lazy-random 2))))
+
+(defn two-heads [v]
+  (every? #(= % :head) v))
+
+(two-heads [:head :head])
+(two-heads [:head :crux])
+
+(count (filter two-heads (partition 2 1 (tosses 20))))
