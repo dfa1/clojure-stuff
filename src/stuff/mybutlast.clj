1ba57a995448ff08a6c0998ab1661b5ad83c8b88
Davide Angelocola <davide.angelocola@gmail.com>
2011-08-20 23:54:22 +0200

    enh: added more altenative versions of mybutlast

--- src/stuff/mybutlast.clj
+++ src/stuff/mybutlast.clj
@@ -3,3 +3,17 @@
 (defn mybutlast [coll]
   "A naive (and slow) implementation of butlast."
   (first (partition (dec (count coll)) coll)))
+
+(defn mybutlast2 [coll]
+  (if (= (count coll) 1) ()
+      (cons (first coll) (mybutlast2 (rest coll)))))
+
+(defn mybutlast3 [coll]
+  (reverse (rest (reverse coll))))
+
+(defn mybutlast4 [coll]
+  (-> coll reverse rest reverse))
+
+
+
+
