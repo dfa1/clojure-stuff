b588683550126f60b339eec6940f9304996a11f7
Davide Angelocola <davide.angelocola@gmail.com>
2011-08-20 23:07:11 +0200

    ref: further simplified uppercase

--- src/stuff/uppercase.clj
+++ src/stuff/uppercase.clj
@@ -2,7 +2,8 @@
 
 (defn uppercase? [string]
   "Returns true if every letter in string is uppercase, else false."
-  (let [characters (seq string)]
-    (and
-     (every? #(Character/isUpperCase %) (filter #(Character/isLetter %) characters)))))
+  (and
+   (not (empty? string))
+   (every? #(Character/isUpperCase %) (filter #(Character/isLetter %) string))))
+
 
