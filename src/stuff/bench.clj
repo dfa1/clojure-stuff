e2e77c79f8f24799aa92b2d4e85b135be19e59f8
Davide Angelocola <davide.angelocola@gmail.com>
2011-08-22 23:27:06 +0200

    ref: doc

--- src/stuff/bench.clj
+++ src/stuff/bench.clj
@@ -1,17 +1,12 @@
 (ns stuff.bench)
 
 (defn bench-fn [n fn]
+  "Returns the average of running fn repeat fn n times."
   (let [startTime (System/currentTimeMillis)]
     (doall (repeatedly n fn)) ; FIXME: dotimes
     (float (/ (- (System/currentTimeMillis) startTime) n))))
 
 (defn bench-report [n fns]
-  "Returns a map of (coll x) -> elapsed with timing of applying args to (coll x)."
+  "Returns a map of fn->avg."
   (zipmap (keys fns) (map #(bench-fn n %) (vals fns))))
 
-
-
-
-
-
-  
