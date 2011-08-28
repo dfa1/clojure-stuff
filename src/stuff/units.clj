90e81658af2aa9059ca9ad0dc96411a0ac70423c
Davide Angelocola <davide.angelocola@gmail.com>
2011-08-28 23:04:44 +0200

    enh: units.clj

---
+++ src/stuff/units.clj
@@ -0,0 +1,12 @@
+;; units library 
+;; inspired by Let Over Lambda example at page 112
+
+(comment 
+  (defunit time s
+    m (60 s)
+    h (60 m)
+    d (24 h)
+    ms (1 / 1000 s)
+    us (1 / 1000 ms))
+  )
+
