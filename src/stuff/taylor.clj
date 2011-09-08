6c64f69012f03bd88bb1777eda12a51b5f820641
Davide Angelocola <davide.angelocola@gmail.com>
2011-09-08 22:10:42 +0200

    ref: naming

--- src/stuff/taylor.clj
+++ src/stuff/taylor.clj
@@ -4,11 +4,11 @@
   "Returns a lazy sequence of powers of x."
   (iterate #(* % x) x))
 
-(defn factorial-seq [n fact]
-  (let [n+1 (inc n)]
+(defn factorial-seq [current factorial]
+  (let [next (inc current)]
     (lazy-seq
-     (cons fact (factorial-seq n+1 (* n+1 fact))))))
-
+     (cons factorial (factorial-seq next (* next factorial))))))
+ 
 (defn factorials []
   "Returns a lazy sequence of factorials numbers."
   (factorial-seq 1 1))
