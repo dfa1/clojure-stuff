2d3c8073a313999c5f40f53cc63bd128c8d83eb3
Davide Angelocola <davide.angelocola@gmail.com>
2011-09-25 16:09:29 +0200

    ref: project metadata

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject clojure-stuff "1.0.0-SNAPSHOT"
-  :description "various clojure stuff"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :description "clojure learning stuff "
+  :dependencies [[org.clojure/clojure "1.3.0"]])
