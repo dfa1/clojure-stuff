abc79b76bdd48ff3cca862fb2359f4720304fe74
Davide Angelocola <davide.angelocola@gmail.com>
2011-10-10 00:24:48 +0200

    enh: doctest

--- src/stuff/doctest.clj
+++ src/stuff/doctest.clj
@@ -2,17 +2,35 @@
 ;; inspired by python
 (ns stuff.doctest)
 
-(defn sum [ & args] 
-  """
+(defn sum  
+  "
 user=> (sum)
 0
 user=> (sum 1)
 1
 user=> (sum 1 2 3)
-1 2 3
-  """
+6
+"
+  [ & args]
   (reduce + args))
 
+(defn samples [s]
+  (map seq (map #(.split % "\n") (remove empty? (re-seq #"user=>.*\n.*" s)))))
+
+(defn dotest [[input, expected]]
+  (let [test (.replace input "user=>" "")
+        actual (eval (read-string test)) ]
+    (assert
+     (=
+      (eval (read-string expected))
+      actual)
+     (format "failed '%s' expected: %s got: %s" test expected actual))))
+
+(defmacro docstring [f]
+  `(doseq [sample# (samples (:doc (meta (var ~f))))]
+     (dotest sample#)))
+
+(docstring sum)
 
 
 
