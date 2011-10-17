0c8db333844b85842b56d2ee4fd14fc6b3ac114f
Davide Angelocola <davide.angelocola@gmail.com>
2011-10-18 00:25:59 +0200

    fix: unit tests

--- test/stuff/test/norm.clj
+++ test/stuff/test/norm.clj
@@ -3,10 +3,10 @@
   (:use [clojure.test]))
 
 (deftest test-with-zero-args
-  (is (= 0 (norm))))
+  (is (= 0.0 (norm))))
 
 (deftest test-with-one-args
-  (is (= 1 (norm 1))))
+  (is (= 1.0 (norm 1))))
 
 (deftest test-with-two-args
   (is (= 2.23606797749979 (norm 1 2))))
